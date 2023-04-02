f01095a9b1a25f10ad27100e00d68bb992f21505
tootouch <wogur379@gmail.com>
2023-04-03 01:54:32 +0900

    build class

--- query_strategies/learning_loss.py
+++ query_strategies/learning_loss.py
@@ -1,1 +1,11 @@
-import torch
+import torch.nn as nn
+
+class LearningLoss(nn.Module):
+    def __init__(self, margin: float = 1.0):
+        pass
+    
+    
+class LossPredictionModule(nn.Module):
+    def __init_(self):
+        pass
+    
