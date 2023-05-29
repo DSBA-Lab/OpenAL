ca720b39df468c605b84638c68f75cc412aa5ccf
tootouch <jaehyuk_heo@korea.ac.kr>
2023-05-29 17:15:29 +0900

    [refactor] load model in create_model

--- models/factory.py
+++ models/factory.py
@@ -1,7 +1,9 @@
 from timm import create_model as _create_model
 
 def create_model(modelname: str, num_classes: int, img_size: int, pretrained: bool = False):
-    if pretrained:
-        return _create_model(modelname, num_classes=num_classes, pretrained=True)
-    else:
+    if modelname in __import__('models').__dict__.keys():
         return __import__('models').__dict__[modelname](num_classes=num_classes, img_size=img_size)
+    else:
+        return _create_model(modelname, num_classes=num_classes, pretrained=pretrained)
+
+        
