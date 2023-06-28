21d151b318b6dd0840f0055892506457971c83c0
tootouch <jaehyuk_heo@korea.ac.kr>
2023-06-28 23:04:02 +0900

    [feat] add PadWithKeepRatio and refactoring

--- datasets/augmentation.py
+++ datasets/augmentation.py
@@ -1,30 +1,24 @@
-from torchvision import transforms
-from omegaconf.dictconfig import DictConfig
+import numbers
+import numpy as np
+import torch
+import torch.nn.functional as F
 
-augments_dict = {
-    'RandomCrop': transforms.RandomCrop,
-    'RandomHorizontalFlip': transforms.RandomHorizontalFlip,
-    'Resize': transforms.Resize
-}
+from torchvision import transforms
 
 def add_augmentation(transform: transforms.Compose, img_size: int, aug_info: list = None):
+    augments_dict = {
+        'RandomCrop': transforms.RandomCrop((img_size, img_size), padding=4),
+        'RandomHorizontalFlip': transforms.RandomHorizontalFlip(),
+        'RandomVerticalFlip': transforms.RandomVerticalFlip(),
+        'Resize': transforms.Resize((img_size, img_size)),
+        'PadWithKeepRatio': PadWithKeepRatio(padding_mode='reflect')
+    }
     # insert augmentations
     if aug_info != None:    
         for aug in aug_info:
-            print(aug)
-            if isinstance(aug, dict) or isinstance(aug, DictConfig):
-                for name, params in aug.items():
-                    if name == 'RandomCrop':
-                        transform.transforms.insert(-1, augments_dict[name]((img_size, img_size), **params))
-                    else:
-                        transform.transforms.insert(-1, augments_dict[name](**params))
-            else:
-                if aug == 'Resize':
-                    transform.transforms.insert(-1, augments_dict[aug]((img_size, img_size)))
-                else:
-                    transform.transforms.insert(-1, augments_dict[aug]())   
+            transform.transforms.insert(-1, augments_dict[aug])   
     else:
-        transform.transforms.insert(-1, augments_dict['Resize']((img_size, img_size)))
+        transform.transforms.insert(-1, augments_dict['Resize'])
     
     return transform
 
@@ -47,3 +41,44 @@ def test_augmentation(img_size: int, mean: tuple, std: tuple):
     ])
 
     return transform
+
+
+def get_padding(image):    
+    # image shape: C x H x W
+    h, w = image.shape[1:]
+    # max size
+    max_size = np.max([h, w])
+    
+    # height and width pad size
+    h_pad = abs(max_size - h) / 2
+    w_pad = abs(max_size - w) / 2
+    
+    # t: top, b: bottom, r: right, l: left
+    t_pad = b_pad = h_pad if h_pad % 1 else h_pad + 0.5
+    l_pad = r_pad = w_pad if w_pad % 1 else w_pad + 0.5
+    
+    # return padding
+    padding = (int(l_pad), int(r_pad), int(t_pad), int(b_pad))
+    return padding
+
+class PadWithKeepRatio(object):
+    def __init__(self, fill=0, padding_mode='constant'):
+        assert isinstance(fill, (numbers.Number, str, tuple))
+        assert padding_mode in ['constant', 'edge', 'reflect', 'symmetric']
+
+        self.fill = fill
+        self.padding_mode = padding_mode
+        
+    def __call__(self, img: torch.Tensor):
+        """
+        Args:
+            img (torch.Tensor): Image to be padded.
+
+        Returns:
+            Padded image (torch.Tensor)
+        """
+        return F.pad(input=img, pad=get_padding(img), mode=self.padding_mode, value=self.fill)
+    
+    def __repr__(self):
+        return self.__class__.__name__ + '(fill={0}, padding_mode={1})'.\
+            format(self.fill, self.padding_mode)
