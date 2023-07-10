9316b7af29f1b0891c6620aa40703b7a610f2028
crimama <crimama2@gmail.com>
2023-07-10 07:23:59 +0000

    [feat] add query strategy

--- query_strategies/__init__.py
+++ query_strategies/__init__.py
@@ -5,5 +5,6 @@ from .margin_sampling import MarginSampling
 from .entropy_sampling import EntropySampling
 from .random import RandomSampling
 from .bald import BALD 
+from .badge import BADGE 
 from .factory import create_query_strategy
 from .make_startset import create_labeled_index, random_select, stratified_random_select
