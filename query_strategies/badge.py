fdcbcd6d76beefb4a977b299e8c582e65f316333
crimama <crimama2@gmail.com>
2023-07-17 15:44:52 +0000

    [fix] generalization for all model : embDim, nLab, pooling

--- query_strategies/badge.py
+++ query_strategies/badge.py
@@ -11,11 +11,6 @@ from torch.utils.data import Dataset, DataLoader
 from .strategy import Strategy,SubsetSequentialSampler
 
 class BADGE(Strategy):
-    '''
-    사용하는 데이터(데이터셋)에 따라 일부 코드 수정 필요 
-    현재 코드 Samsung 데이터에 맞춰져 있음 
-    수정 대상 : emBDim, nLab 
-    '''
     def __init__(
         self, model, n_query: int, labeled_idx: np.ndarray, 
         dataset: Dataset, batch_size: int, num_workers: int, num_mcdropout: int = 10):
@@ -47,34 +42,24 @@ class BADGE(Strategy):
             num_workers = self.num_workers
         )
         
-        embDim = model.head.in_features
-        # embDim = model.linear.in_features # <- benchmark 용 
-        device = next(model.parameters()).device
-        model.eval()
+        embDim = model.num_features
+        device = next(model.parameters()).device 
         nLab = len(dataloader.dataset.label_info['label'].unique()) #라벨 수 : 4
-        # nLab = len(dataloader.dataset.class_to_idx) # <- CIFAR 10 벤치마크 용 
-        # nLab = len(np.unique(dataloader.dataset.labels)) # <- SVHN 벤치마크 용 
         embedding = np.zeros([len(sampler), embDim * nLab])
         
+        model.eval()
         with torch.no_grad():
             for idx, (imgs,y) in enumerate(dataloader):
                 idxs = np.arange(idx* dataloader.batch_size, (idx+1) * dataloader.batch_size)
                 
                 x = model.forward_features(imgs.to(device))
-                emb = model.head.global_pool(x)
-                emb = model.head.drop(emb)
-                out = model.head.fc(emb)
-                '''
-                
-                # emb = model.global_pool(x)
-                # emb = emb.view(emb.size(0),-1)
-                # out = model.linear(emb)
-                '''
+                emb = pooling_embedding(x)
+                out = model.forward_head(x)                                 
                 
                 emb = emb.data.cpu().numpy()
                 batchprobs = F.softmax(out, dim=1).data.cpu().numpy() #Softmax 
                 
-                maxInds = np.argmax(batchprobs,1) #각 데이터 별 softmax 최대 값 
+                maxInds = np.argmax(batchprobs,1) # predicted label of model for each data 
                 for j in range(len(y)):
                     for c in range(nLab):
                         if c == maxInds[j]:
@@ -90,17 +75,22 @@ class BADGE(Strategy):
         unlabeled_idx = np.where(self.labeled_idx==False)[0] 
         select_idx = unlabeled_idx[chosen]        
         return select_idx
-    
+
+
 @torch.no_grad()
-def init_centers(X, K):
+def init_centers(X: torch.Tensor, K: int = None):
+    
+    # K-means ++ initializing 
     embs = torch.Tensor(X)
     ind = torch.argmax(torch.norm(embs, 2, 1)).item()
     embs = embs.cuda()
+    
     mu = [embs[ind]]
     indsAll = [ind]
     centInds = [0.] * len(embs)
     cent = 0
-    print('#Samps\tTotal Distance')
+    
+    # Sampling 
     while len(mu) < K:
         if len(mu) == 1:
             D2 = torch.cdist(mu[-1].view(1,-1), embs, 2)[0].cpu().numpy()
@@ -109,15 +99,32 @@ def init_centers(X, K):
             for i in range(len(embs)):
                 if D2[i] >  newD[i]:
                     centInds[i] = cent
-                    D2[i] = newD[i]
-        print(str(len(mu)) + '\t' + str(sum(D2)), flush=True)
-        if sum(D2) == 0.0: pdb.set_trace()
+                    D2[i] = newD[i]            
         D2 = D2.ravel().astype(float)
+        
         Ddist = (D2 ** 2)/ sum(D2 ** 2)
         customDist = stats.rv_discrete(name='custm', values=(np.arange(len(D2)), Ddist))
         ind = customDist.rvs(size=1)[0]
+        
         while ind in indsAll: ind = customDist.rvs(size=1)[0]
+        
         mu.append(embs[ind])
         indsAll.append(ind)
         cent += 1
     return indsAll
+
+
+def pooling_embedding(x):
+    '''
+    dim : target dimension for pooling 
+    mean : pooling for grad embedding 
+    '''
+    if np.argmax(list(x.shape)) == 1: # x shape : NHWC -> for Resnet 
+        dim = (2,3)
+        emb = x.mean(dim)
+    elif np.argmax(list(x.shape)) == 3:# x shape : NCHW -> for Swin-Transformer 
+        dim = (1,2)
+        emb = x.mean(dim)
+    elif len(x.shape) == 3: # x shape : NTC -> for ViT 
+        emb = x[:,0,:] # cls token 
+    return emb 
