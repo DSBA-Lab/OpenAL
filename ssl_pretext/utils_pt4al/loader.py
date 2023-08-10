02f100501ba92705395fc9aaddd3bae57f902010
tootouch <jaehyuk_heo@korea.ac.kr>
2023-08-11 00:50:36 +0900

    [refactor] dataset refactoring

--- ssl_pretext/utils_pt4al/loader.py
+++ ssl_pretext/utils_pt4al/loader.py
@@ -5,23 +5,25 @@ import random
 
 
 class RotationDataset(Dataset):
-    def __init__(self, dataset, is_train=True, transform=None):
+    def __init__(self, dataset, is_train=True):
         self.dataset = dataset
         self.is_train = is_train
-        self.transform = transform
+        self.rotations = [0,1,2,3]
                 
     def __len__(self):
         return len(self.dataset)
 
     def __getitem__(self, idx):
-        img, target = self.dataset[idx] 
+        img, _ = self.dataset[idx] 
         
-        img1, img2, img3 = torch.rot90(img, 1, [1,2]), torch.rot90(img, 2, [1,2]), torch.rot90(img, 3, [1,2])
-        imgs = [img, img1, img2, img3]
-        rotations = [0,1,2,3]
-        random.shuffle(rotations)
-
         if self.is_train:
-            return [imgs[rotations[0]], imgs[rotations[1]], imgs[rotations[2]], imgs[rotations[3]]], [rotations[0], rotations[1], rotations[2], rotations[3]]
-        else:
-            return [imgs[rotations[0]], imgs[rotations[1]], imgs[rotations[2]], imgs[rotations[3]]], [rotations[0], rotations[1], rotations[2], rotations[3]], idx
+            target = random.choice(self.rotations)
+            input = torch.rot90(input=img, k=target, dims=[1,2])
+
+        else:        
+            target = self.rotations
+            input = [img]
+            for t in target[1:]:
+                input.append(torch.rot90(input=img, k=t, dims=[1,2]))
+
+        return input, target
