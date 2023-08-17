f2516b74ceae1b86471b436572424cab0410b5c4
tootouch <jaehyuk_heo@korea.ac.kr>
2023-08-17 16:16:29 +0900

    [feat] add VarRatioSampliing, MeanSTD in init

--- query_strategies/__init__.py
+++ query_strategies/__init__.py
@@ -7,5 +7,7 @@ from .random import RandomSampling
 from .bald import BALD 
 from .badge import BADGE
 from .pt4al import PT4AL, PT4LeastConfidence, PT4MarginSampling, PT4EntropySampling
+from .meanstd_sampling import MeanSTDSampling
+from .varratio_sampling import VarRatioSampling
 from .factory import create_query_strategy
 from .make_startset import create_labeled_index, random_select, stratified_random_select, batch_select, get_batch_params
