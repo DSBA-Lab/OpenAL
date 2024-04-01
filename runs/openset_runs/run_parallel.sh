659348125f0a1012681f558ca3ce072656f2dba5
tootouch <wogur379@gmail.com>
2024-04-01 21:39:59 +0900

    chore: update configs

--- runs/openset_runs/run_parallel.sh
+++
@@ -1,6 +0,0 @@
-bash runs/openset_runs/run.sh CIFAR10 0.1 ./results_openset-test 0 &
-tmux split-window -v 'bash runs/openset_runs/run.sh CIFAR10 0.2 ./results_openset-test 1' &
-tmux split-window -v 'bash runs/openset_runs/run.sh CIFAR10 0.4 ./results_openset-test 2' &
-tmux split-window -v 'bash runs/openset_runs/run.sh CIFAR10 0.6 ./results_openset-test 3' &
-
-tmux select-layout even-vertical &
